04e4939aa5ad94e540fa1d6ef63d25ac0418ba17
bheem-works <shokokomichan098@gmail.com>
2025-07-16 23:14:41 +0545

    commit

---
+++ july_eleven.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function july_eleven() {
+  return (
+    <div className='container'>
+      <div className="start">
+        <p>Today was the sunny days. And the most funny thing's is that i waked up with the mom slap.</p>
+        She just slaped me and i waked up and realize that ohh i have to go the college. And however 
+        i manage to escaped from the bed and i waked up and take a hair shower and be ready and went
+        for the college.
+      </div>
+
+      <div className="event">
+        <p>My friend arrive at today. and yeh i feel like i spend my time in college in so fast.
+        But actually time was that. I just feel today college time went so fast. but actually it doesnot. 
+        You can call a slap day today also. haha it sounds funny but it is nice for me. 
+
+        And as always don't know what is holding me. 
+        As always think i am going to do that. At this <b>Time</b> but i am unable to make that happens. 
+        is this what we called love some times happiness. And some times sad. But i have faced so much times 
+        sadness and i am so lazy and many thing's are holding me don't get the idea what do. 
+        Anyways that's it there are many thing's that i have to talk about i would like to talk that thing's later not now. 
+        See yah!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default july_eleven
