a386be42110345ade07a0f69e185f0a3ffa6925e
bheem-works <shokokomichan098@gmail.com>
2025-07-21 23:47:51 +0545

    Push this journal file

---
+++ JournalTwentyOne.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function JournalTwentyOne() {
+  return (
+    <div className='container'>
+      <div className="start">
+        <h1>Journal Entry 21</h1>
+        <p>Today, was pretty fine and hot day that's it. 
+        <br />
+        Something's really broke my heart which i am not in mood to talk with that stuff. 
+        I wil talk later about that stuff.
+        </p>
+        <div className="even">
+            Event* 
+            Today night is pretty painful for me. How can i tell you that today was pretty painfull for me. 
+            You will laught at me is it? but yeh today was painfule for me and yeh i don't want to talk about it
+            My highness has said something's very deeply and i don't know how can i explain that. 
+            But that words just litreally hurt me. that's it. 
+            The dailouge of today is that . 
+            <i>
+                "You can add the value to people lives but you can not be the valuable for them"
+            </i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default JournalTwentyOne
---
+++ JulyTwenty.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+function JulyTwenty() {
+  return (
+    <div className="contianer">
+    <div className="start">
+      <p>
+        Today day was fine and nice weather by the way not so cold and not so hot the nice weather. And yeh i think i have read so much. 
+        But i also don'w know what i do but i think i just learn something's new and that's fine. I am just trying and pushing my self.
+        Little by little but this is enough for now i guess so. 
+        Well the Mr.mnesma told me to do that the todolist thing's but i was not able to do that. May be that is hard for me or you can 
+        say that i don't have the knoweldge about the class properly and also get so many confuse and the questions. 
+        Sometimes i feel like there should be the person or the real mentor who can teach me in the real time. But yeh that is now possible. 
+        For now.. So i have to to do and being stuck like this until i found some real mentor or until i become the master on it. 
+        So today was kind of the nice day not so but yeh nice the i might have to say that insane youtube name his name is jamesshclz. 
+        And yeh i am obviosuly listening his background rain musics. Which is so satisfying to me right now. 
+      </p> 
+    </div>
+    
+    <div className="event">
+      <p>Event * 
+      What has happened today and what thing's can i called the event. First thing's i just read but don't know what i read. And i am thingking 
+      i need to write a book of something's but instead writting a book for now i need to understand this Javascript script. Well there is the 
+      thing's that i just get to knwo that js is not the weird just people have the lack of logics and the understanding problems which made 
+      them to think them this programming so weird that's it. 
+      And yeh today's i just eat the thupa yesterday i have also eat the thupa and the tommorow i am also going to eat the thupa. 
+    </p> 
+    </div>
+    </div>
+  )
+}
+
+export default JulyTwenty
